697558678426d33bb6f1504a49ea2a8ed306c58a
@heyaibek <heyaibek@gmail.com>
2024-07-02 00:13:40 +0200

    chore: format code

--- Package.swift
+++ Package.swift
@@ -4,16 +4,16 @@
 import PackageDescription
 
 let package = Package(
-    name: "ExercismSwift",
-		platforms: [
-			.iOS(.v12),
-			.macOS(.v10_13),
-		],
-    products: [
-        .library(name: "ExercismSwift", targets: ["ExercismSwift"]),
-    ],
-    targets: [
-        .target(name: "ExercismSwift"),
-        .testTarget(name: "ExercismSwiftTests", dependencies: ["ExercismSwift"]),
-    ]
+	name: "ExercismSwift",
+	platforms: [
+		.iOS(.v12),
+		.macOS(.v10_13),
+	],
+	products: [
+		.library(name: "ExercismSwift", targets: ["ExercismSwift"]),
+	],
+	targets: [
+		.target(name: "ExercismSwift"),
+		.testTarget(name: "ExercismSwiftTests", dependencies: ["ExercismSwift"]),
+	]
 )
